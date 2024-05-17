94de773773229a61f1d3bc396e4c1e6862188808
刘祥超 <iwind.liu@gmail.com>
2024-05-17 18:30:33 +0800

    修改copyright

--- internal/utils/agents/agent.go
+++ internal/utils/agents/agent.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Liuxiangchao iwind.liu@gmail.com. All rights reserved. Official site: https://goedge.cn .
+// Copyright 2022 GoEdge goedge.cdn@gmail.com. All rights reserved. Official site: https://goedge.cn .
 
 package agents
 
